e822301a8da4d2ee2c93f52d230bd70c0a1c5e0c
TiparpronS <Sukanyat2022a@gmail.com>
2022-12-22 08:40:17 -0500

    add

--- contact.js
+++ contact.js
@@ -1,13 +1,22 @@
-function submitWholeForm(){
-    var firstName = document.forms['myFormJvScript']['Fname'].value;
-    if(firstName.length<2){
-        alert("Not valid")
 
-    }
 
-    var lastName = document.forms['myFormJvScript']['Lname'].value;
-    if(lastName.length<2){
-        alert("Not valid")
+function submitWholeForm() {
 
-    }
+ 
+  
+  var firstName = document.forms['myFormJvScript']['Fname'].value;
+  
+  if (firstName.length < 2) {
+  alert('First name is not valid');
+
+  }
+
+  var lastName = document.forms['myFormJvScript']['Lname'].value;
+  if (lastName.length < 2) {
+   alert('Last name is not valid');
+  }
+
+
+
+  
 }
